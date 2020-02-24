820c8b766f02209b5942fe94b496292e8961fa31
Tanya_Sinha <58481752+TanyaSinha550@users.noreply.github.com>
2020-02-25 00:03:30 +0530

    Update query_imp1.sql

--- query_imp1.sql
+++ query_imp1.sql
@@ -18,3 +18,10 @@ where a.dept_code =b.dept_code and a.staff_sal =(select max(staff_sal) from staf
 select a.staff_name , a.staff_sal ,b.dept_code, b.dept_name
 from staff_master a, department_master b
 where a.dept_code =b.dept_code and a.staff_sal in (select max(staff_sal) from staff_master group by dept_code);
+
+----------Diaplay alternate records using rownum 
+----------where cannot be directly used with rwonum so we use outer query select * from 
+select * from 
+(select rownum rn ,staff_code ,dept_code from staff_master 
+order by dept_code)
+where mod(rn,2)!=0;
